11512322a6da789adf6bf5b7c8e3bc4ca82f5824
Del-Lins <deolins1325@gmail.com>
2024-04-16 20:48:27 -0300

    Landing Page do Venom

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bi-x');
+  navlist.classList.toggle('open');
+};
